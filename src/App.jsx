c30bb4649fc16257ab3940cf64a7b3d241fb0168
iamnls <iamnls@hotmail.com>
2024-10-25 07:22:31 +0545

    changing the color of text

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,57 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+// import './App.css'
+// import Navbar from './components/Navbar'
+
+// function App() {
+//   //const [color, setColor] = useState("blue")
+//   const [mode, setMode] = useState('light')
+//   const [text, setText] = useState('enable dark mode')
+//     const toggleMode=()=>{
+//   if (mode=='light'){
+//     setMode('dark')
+//     setText('enable light mode')
+//   }
+//   else{
+//     setMode('light')
+//     setText('enable dark mode')
+//   }
+//   }
+
+//   return (
+//     <>
+//       <Navbar title="this is header" mode={mode} text={text} toggleMode={toggleMode}/>
+
+//       <h1>Vite + React</h1>
+//       {/*<div className="card">
+//         <button onClick={() => setColor("red")}>
+//           please click this page  {color}
+//         </button>
+
+//       </div>*/}
+
+//     </>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
-  //const [color, setColor] = useState("blue")
-  const [mode, setMode] = useState('light')
-  const [text, setText] = useState('enable dark mode')
-    const toggleMode=()=>{
-  if (mode=='light'){
-    setMode('dark')
-    setText('enable light mode')
-  }
-  else{
-    setMode('light')
-    setText('enable dark mode')
-  }
-  }
+  const [color, setColor] = useState('blue')
+
+  const handleSetColor = (newColor) => {
+    setColor(newColor);
+  };
 
   return (
-    <>
-      <Navbar title="this is header" mode={mode} text={text} toggleMode={toggleMode}/>
-
-      <h1>Vite + React</h1>
-      {/*<div className="card">
-        <button onClick={() => setColor("red")}>
-          please click this page  {color}
-        </button>
-
-      </div>*/}
-
-    </>
-  )
+    <div>
+      <Navbar setColor={handleSetColor} />
+      <p style={{ color: color }}>This text changes color!</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,66 @@
-import React from 'react'
-import 'bootstrap'
-import 'bootstrap/scss/bootstrap.scss';
-const Navbar = (props) => {
-    return (
-        <nav class={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">{props.title}</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></hr></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              {/*<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-              <button class="btn btn-outline-success" type="submit">Search</button>*/}
+// import React from 'react'
+// import 'bootstrap'
+// import 'bootstrap/scss/bootstrap.scss';
+// const Navbar = (props) => {
+//     return (
+//         <nav class={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+//         <div class="container-fluid">
+//           <a class="navbar-brand" href="#">{props.title}</a>
+//           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//             <span class="navbar-toggler-icon"></span>
+//           </button>
+//           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+//               <li class="nav-item">
+//                 <a class="nav-link active" aria-current="page" href="#">Home</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link" href="#">Link</a>
+//               </li>
+//               <li class="nav-item dropdown">
+//                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                   Dropdown
+//                 </a>
+//                 <ul class="dropdown-menu">
+//                   <li><a class="dropdown-item" href="#">Action</a></li>
+//                   <li><a class="dropdown-item" href="#">Another action</a></li>
+//                   <li><hr class="dropdown-divider"></hr></li>
+//                   <li><a class="dropdown-item" href="#">Something else here</a></li>
+//                 </ul>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+//               </li>
+//             </ul>
+//             <form class="d-flex" role="search">
+//               {/*<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+//               <button class="btn btn-outline-success" type="submit">Search</button>*/}
               
-            </form>
-            <button className='btn btn-success'onClick={props.toggleMode}>{props.text}</button>
-          </div>
-        </div>
-      </nav>
+//             </form>
+//             <button className='btn btn-success'onClick={props.toggleMode}>{props.text}</button>
+//           </div>
+//         </div>
+//       </nav>
 
-    )
+//     )
+// }
+// export default Navbar
+
+import React from 'react';
+
+function Navbar({ setColor }) {
+
+  const handleColorChange = () => {
+    const newColor = prompt("Enter a color:");  // Prompt for user input
+    setColor(newColor);  // Pass new color to parent component or state
+  };
+
+  return (
+    <nav>
+      <h1>Navbar</h1>
+      <button onClick={handleColorChange}>Change Color</button>
+    </nav>
+  );
 }
+
 export default Navbar
